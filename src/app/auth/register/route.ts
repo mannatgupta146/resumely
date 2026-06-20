e8b0580c6d5a0e8763cb756e1c633160762b3ebb
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-20 17:47:58 +0530

    package: Update package dependencies and fix vulnerabilities

--- src/app/auth/register/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function GET(res: NextRequest) {
-    return NextResponse.json({ 
-        message: "Hello from the GET method!" 
-    })
-}
